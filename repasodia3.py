50e23131deab58fab339066c2b5578c9dafc4346
codigosinbugs <diegofelipetv97@gmail.com>
2025-09-21 19:34:37 +0200

    Agrega repaso del Dia 3

---
+++ repasodia3.py
@@ -0,0 +1,24 @@
+# Repaso día 3 
+# Tipos de datos
+numero_entero = 777
+saludo_texto = "Hola CodigoSinBugs, bienvenido a tu primer repaso de Python "
+es_verdad = True
+
+# Ejemplo 1 imprimir 1x1
+# Salida en líneas separadas
+print("Ejemplo 1:")
+print(numero_entero)
+print(saludo_texto)
+print(es_verdad)
+
+
+# Ejemplo 2: Imprimier todas juntas en una linea
+# Los valores se separan con un espacio
+print("\nEjemplo 2:")
+print(numero_entero, saludo_texto, es_verdad)
+
+
+# Ejemplo 3: Imprimir todas juntas pero en lineas separadas
+# Usando el caracter de salto de línea '\n'
+print("\nEjemplo 3:")
+print(f"Número: {numero_entero}\nSaludo: {saludo_texto}\nVerdad: {es_verdad}")
