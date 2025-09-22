5bcbdd4be99871a7e6ee86df0c1797a1c3336cd5
codigosinbugs <diegofelipetv97@gmail.com>
2025-09-22 13:58:47 +0200

    Se creó calculadora-compleja-día4.py es un proyecto de una calculadora cientifica en proceso mientras voy aprendiendo cada vez más a programar código

--- mini-calculadora-dia3.py
+++ mini-calculadora-dia3.py
@@ -1,21 +1,21 @@
 #------------------------->MINI-CALCULADORA<-------------------#
 primer_numero = input("Escribe el primer número: ")
-segundo_numero = input("Escribe el segundo número")
+segundo_numero = input("Escribe el segundo número: ")
 
 primer_numero_int = int(primer_numero)
-segundo_numero_numero_int = int(segundo_numero)
+segundo_numero_int = int(segundo_numero)
 
 # Suma de los dos números 
-resultado_suma = primer_numero_int + segundo_numero_numero_int
+resultado_suma = primer_numero_int + segundo_numero_int
 
 # Resta de los dos números 
-resultado_resta = primer_numero_int - segundo_numero_numero_int
+resultado_resta = primer_numero_int - segundo_numero_int
 
 # Multiplicación de los dos números
-resultado_multiplicacion = primer_numero_int * segundo_numero_numero_int
+resultado_multiplicacion = primer_numero_int * segundo_numero_int
 
 # División de los dos números
-resultado_division = primer_numero_int / segundo_numero_numero_int
+resultado_division = primer_numero_int / segundo_numero_int
 
 
 print(f"La suma es: {resultado_suma}")
