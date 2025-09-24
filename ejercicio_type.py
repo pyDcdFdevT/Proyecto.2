967d600fe5f4d4ab435486b800be631bfe676e53
pyDcdFdevT <diegofelipetv97@gmail.com>
2025-09-25 00:20:42 +0200

    Limpiamos la estructura de archivos del proyecto

---
+++ ejercicio_type.py
@@ -0,0 +1,25 @@
+# EJERCICIO DE TIPO DE DATOS
+
+#-----------EJERCICIO 0--------------
+# Pídele un número al usuario y guarda su respuesta en una variable
+#respuesta_usuario = input("Escribe un número: ")
+
+# Muestra el tipo de dato que Python le asigna a la variable 'respuesta_usuario'
+#print("El tipo de dato de tu respuesta es:")
+#print(type(respuesta_usuario))
+
+#-----------EJERCICIO 1--------------
+# Pídele al usuario su edad y su nombre
+#nombre = input("¿Cuál es tu nombre? ")
+#edad_texto = input("¿Cuántos años tienes? ")
+
+# Intenta imprimir una frase combinando texto y el número de la edad
+#print("Hola " + nombre + ", tu edad es " + edad_texto)
+
+#-----------EJERCICIO 2--------------
+# Convierte la edad a número para poder hacer un cálculo
+#edad_numero = int(edad_texto)
+#edad_futura = edad_numero + 1
+
+# Imprime el resultado
+#print(f"El próximo año, tendrás {edad_futura} años.")
